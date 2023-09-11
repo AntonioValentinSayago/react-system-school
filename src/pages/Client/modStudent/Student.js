412c2ccc1edc6819f46932a17a0f96c65e880e4a
MontserratBenicio <monce.benicio@gmail.com>
2023-09-11 11:51:49 -0600

    primer cambio

---
+++ src/pages/Client/modStudent/Student.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CompStudent from '../../../Components/Student/CompStudent'
+
+export function Student() {
+  return (
+    <div>
+        <CompStudent/>
+    </div>
+  )
+}
---
+++ src/routes/Student/rtStudent.js
@@ -0,0 +1,11 @@
+import { Student } from '../../pages/Client/modStudent/Student'
+
+const routesStudent = [
+    {
+        path:"/student",
+        component: Student,
+        exact: true,
+    },
+]
+
+export default routesStudent;
