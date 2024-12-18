695c81fccabd11ea458684d39fe77bc97f5e49b0
AntonioValentinSayago <cesar_anto30@hotmail.com>
2024-12-17 21:09:46 -0600

    fix:structure - new options

--- src/plugins/args.plugin.ts
+++ src/plugins/args.plugin.ts
@@ -20,6 +20,18 @@ export const yarg = yargs(process.argv)
         default: false,
         describe:''
     })
+    .option('n', {
+        alias: 'name',
+        type: 'string',
+        default: 'multiplication-table',
+        describe: 'File name'
+    })
+    .option('d', {
+        alias: 'destination',
+        type: 'string',
+        default: 'outputs',
+        describe: 'File destination'
+    })
     .check((argv, options) => {
 
         if (argv.b < 1 ) throw 'Error: base must be a number'        
