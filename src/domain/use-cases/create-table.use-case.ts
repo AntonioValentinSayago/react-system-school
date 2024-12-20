b0dca96cca2f78f8efea17eb568c3608068c92fd
AntonioValentinSayago <cesar_anto30@hotmail.com>
2024-12-19 19:53:15 -0600

    fix: Correcion del bug, al crear un archivo

--- src/domain/use-cases/create-table.use-case.ts
+++ src/domain/use-cases/create-table.use-case.ts
@@ -18,7 +18,9 @@ export class CreateTable implements CreateTableUseCase {
         let outputMessage = ''
         for (let i = 0; i <= limit; i++) {
             outputMessage += `${base} x ${i} = ${ base * i }\n`
+            if( i < limit ) outputMessage +=  '\n';
         }
+
 
         return outputMessage;
     }
